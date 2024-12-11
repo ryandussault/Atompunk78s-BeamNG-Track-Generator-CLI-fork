70b79a7d92fc9b8a5132fdb91021bac6dc411921
ryandussault <ryan.dussault@hotmail.com>
2024-12-11 17:40:06 -0500

    fixed runaway while loop
    improved logic prevents the track from going below ground while still being able to generate, no matter the start height

--- main_cli.py
+++ main_cli.py
@@ -357,7 +357,7 @@ while not acceptableTrack: #makes sure track doesn't go below 0 height
           if parameters["startHeight"] + currentHeight > 0:
               currentFileString += addPiece()
           else:
-              acceptableTrack = False
+              currentHeight = 1
     if not acceptableTrack and parameters["showDebugMessages"]:
       currentFileString = ""
       currentHeight = 0
